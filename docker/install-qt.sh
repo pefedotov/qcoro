c05bd7e46315c357c12c5de6af9192fae64bfcc7
Daniel Vrátil <dvratil@kde.org>
2026-03-31 23:46:48 +0200

    Fix Qt5 support

--- docker/install-qt.sh
+++ docker/install-qt.sh
@@ -13,8 +13,14 @@ if [[ -n "${qt_modules}" ]]; then
     opt_modules="--modules ${qt_modules}"
 fi
 
+if [[ $(echo "${qt_version}" | cut -d'.' -f1) == "5" ]]; then
+    arch="gcc_64"
+else
+    arch="linux_gcc_64"
+fi
+
 echo "Installing Qt ${qt_version}"
 echo "Modules: ${qt_modules}"
 echo "Archives: ${qt_archives}"
-aqt install-qt -O /opt/qt linux desktop "${qt_version}" linux_gcc_64 ${opt_archives} ${opt_modules}
+aqt install-qt -O /opt/qt linux desktop "${qt_version}" "${arch}" ${opt_archives} ${opt_modules}
 echo "Done."
